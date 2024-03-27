25bdb1ca275464bc90d39a1830763f554e900222
AyaAlaa <Ayaalaa010920@gmail.com>
2024-03-27 14:40:48 +0400

    project setup

--- lib/ui/search/views/search_view.dart
+++ lib/ui/search/views/search_view.dart
@@ -7,7 +7,7 @@ class SearchView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.cyan,
+      color: Colors.brown,
     );
   }
 }
