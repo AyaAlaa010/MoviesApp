d5b4442fa307bffe9fb8d65a72eaf9e016a39d2a
AyaAlaa <133635391+laylaaa1@users.noreply.github.com>
2024-03-29 18:08:36 +0400

    work on home  ui

--- lib/ui/home/views/home_view.dart
+++ lib/ui/home/views/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../../../core/config/app_constants/constants.dart';
 import '../../../core/config/styles/app_colors.dart';
 import '../widgets/new_releases_widget.dart';
 
@@ -11,7 +12,61 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return
       Column(
-        children: [],
+      children: [
+        const Expanded(
+          flex: 5,
+          child: Stack(
+            children: [],
+          ),
+        ),
+        Expanded(
+            flex: 3,
+            child: Container(
+              color: AppColors.onPrimaryColor,
+              padding:const  EdgeInsets.only(top: 15,bottom: 15,left: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+
+                children: [
+
+                  Text("New Releases ",style: Constants.theme.textTheme.bodySmall,),
+
+                  Expanded(
+                    child: ListView.builder(
+                      itemBuilder: (context, index) {
+                        return  const NewReleasesWidget();
+                      },
+                      itemCount: 10,
+                      scrollDirection: Axis.horizontal,
+                    ),
+                  ),
+                ],
+              ),
+            )),
+      const   SizedBox(height: 30,),
+        Expanded(
+            flex: 3,
+            child: Container(
+              padding:const  EdgeInsets.only(top: 15,bottom: 15,left: 15),
+
+              color: AppColors.onPrimaryColor,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Recommended",style: Constants.theme.textTheme.bodySmall,),
+                  Expanded(
+                    child: ListView.builder(
+                      itemBuilder: (context, index) {
+                        return  const NewReleasesWidget();
+                      },
+                      itemCount: 10,
+                      scrollDirection: Axis.horizontal,
+                    ),
+                  ),
+                ],
+              ),
+            ))
+      ],
     );
   }
 }
--- lib/ui/home/widgets/new_releases_widget.dart
+++ lib/ui/home/widgets/new_releases_widget.dart
@@ -4,21 +4,26 @@ class NewReleasesWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return   Stack(
-      children: [
-        Image.asset("assets/images/movie_img.png"),
-        Container(
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: Image.asset("assets/images/add_background.png").image
-            )
-          ),
-          child: const Icon(Icons.add,color: Colors.white,size: 30,),
+    return   Padding(
+      padding:const  EdgeInsets.only(top: 10,right: 8),
+      child: Stack(
+        alignment: Alignment.topLeft,
+        children: [
+          Image.asset("assets/images/movie_img.png",fit: BoxFit.cover,),
+          Container(
+            height: 33,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: Image.asset("assets/images/add_background.png",fit: BoxFit.cover).image
+              )
+            ),
+            child: const Icon(Icons.add,color: Colors.white,),
 
-        )
+          )
 
-      ],
+        ],
+      ),
     );
   }
 }
